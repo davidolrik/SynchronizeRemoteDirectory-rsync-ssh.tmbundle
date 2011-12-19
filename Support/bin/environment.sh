061234a84c81478002123781f46b1f2670f29973
David Jack Wange Olrik <david@grepmasters.com>
2011-12-19 23:42:55 +0100

    Added Growl support
    Update to work with TextMate 2 and Growl

--- Support/bin/environment.sh
+++ Support/bin/environment.sh
@@ -2,3 +2,33 @@
 
 export CONFIG_FILE="$TM_PROJECT_DIRECTORY/.tm_sync.config"
 
+if [[ -x /usr/local/bin/growlnotify ]]; then
+    export GROWLNOTIFY="/usr/local/bin/growlnotify"
+elif [[ -x ~/bin/growlnotify ]]; then
+    export GROWLNOTIFY="~/bin/growlnotify"
+fi
+
+function print_message {
+    TITLE="$1"
+    MESSAGE="$2"
+
+    if [[ -n "$GROWLNOTIFY" ]]; then
+        $GROWLNOTIFY -a TextMate -t "${TITLE}" -m "${MESSAGE}"
+    else
+        echo "${TITLE}: ${MESSAGE}"
+    fi    
+}
+
+function error_message {
+    TITLE="$1"
+    MESSAGE="$2"
+
+    if [[ -n "$GROWLNOTIFY" ]]; then
+        $GROWLNOTIFY -a TextMate -t "${TITLE}" -m "${MESSAGE}" -p 2 -s
+    else
+        echo "${TITLE}: ${MESSAGE}"
+    fi    
+}
+
+
+
--- Support/templates/config.sh
+++ Support/templates/config.sh
@@ -9,8 +9,11 @@ REMOTE_USER="username"
 REMOTE_PATH="/remote/directory"
 
 # Here you can parse options on to rsync if you e.g. want to exclude files
-RSYNC_OPTIONS="--exclude blib --exclude _build --exclude Makefile --exclude Build"
+RSYNC_OPTIONS="--exclude=.git --exclude=blib --exclude=_build --exclude=Makefile --exclude=Build --delete"
 
 # Use the rsync option below with caution, as it replaces the remote directory
 # with the contents of your project.
-#RSYNC_OPTIONS="--delete"
+# RSYNC_OPTIONS="--delete"
+
+# Use this option to run any command on the remote end after a successful copy
+# REMOTE_POST_COMMAND="make && make install"
